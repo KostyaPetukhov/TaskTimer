e3ec6573b3153b97e16859073a3f2a98b7f71242
Kostya Petukhov <kosyak971@gmailcom>
2022-06-16 18:19:38 +0300

    Refactored timer styling

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -1,16 +1,59 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 import moment from 'moment';
 
-import { TextField, Paper, Button, Typography } from '@mui/material';
-import './timer.css';
+import TextField from '@mui/material/TextField';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+	timer: {
+		marginTtop: 10,
+		maxWidth: 1000,
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		padding: 20,
+	},
+	taskName: {
+		width: 250,
+		textAlign: 'center',
+	},
+	timerClock: {
+		margin: '30px 0',
+		width: 200,
+		height: 200,
+		borderRadius: 40,
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	button: {
+		width: 75,
+	},
+	inputStyle: {
+		color: '#1976d2 !important',
+		textAlign: 'center',
+	},
+});
+
+const formatTime = (totalMilliseconds) => {
+	const minutesSeconds = moment.utc(totalMilliseconds).format('mm:ss');
+	const hours = moment.duration(totalMilliseconds).asHours().toFixed();
+	const parsedHours = hours <= 9 ? '0' + hours : hours;
+	const formatted = `${parsedHours}:${minutesSeconds}`;
+	return formatted;
+};
 
 const Timer = () => {
-	const active = localStorage.getItem('timerActive') ? true : false;
+	const active = !!localStorage.getItem('startTime');
+	const startTime = JSON.parse(localStorage.getItem('startTime'));
 	const [timerActive, setTimerActive] = useState(active);
 	const [taskTime, setTaskTime] = useState('00:00:00');
+	const classes = useStyles();
 
 	useEffect(() => {
-		const startTime = JSON.parse(localStorage.getItem('startTime'));
 		if (timerActive) {
 			const addSecondInterval = setInterval(() => {
 				const currentTime = Date.now();
@@ -24,38 +67,24 @@ const Timer = () => {
 		}
 	}, [timerActive]);
 
-	const formatTime = (totalMilliseconds) => {
-		const minutesSeconds = moment.utc(totalMilliseconds).format('mm:ss');
-		const hours = moment.duration(totalMilliseconds).asHours().toFixed();
-		const formatted =
-			hours <= 9
-				? '0' + hours + ':' + minutesSeconds
-				: hours + ':' + minutesSeconds;
-
-		return formatted;
-	};
-
 	const handleStartTimer = () => {
 		setTimerActive(true);
-		localStorage.setItem('timerActive', true);
 		const start = Date.now();
 		localStorage.setItem('startTime', JSON.stringify(start));
 	};
 
 	const handleStopTimer = () => {
 		setTimerActive(false);
-		localStorage.removeItem('timerActive');
-		const startTimeMS = JSON.parse(localStorage.getItem('startTime'));
-		const startTime = moment(startTimeMS).format('hh:mm:ss');
+		const start = moment(startTime).format('hh:mm:ss');
 		const finishTimeMS = Date.now();
-		const finishTime = moment().format('hh:mm:ss');
-		const spendTimeMS = finishTimeMS - startTimeMS;
+		const finishTime = moment(finishTimeMS).format('hh:mm:ss');
+		const spendTimeMS = finishTimeMS - startTime;
 		const spendTime = formatTime(spendTimeMS);
 		setTaskTime('00:00:00');
 
 		console.log(
 			'start: ',
-			startTime,
+			start,
 			'finish: ',
 			finishTime,
 			'spend time: ',
@@ -64,33 +93,35 @@ const Timer = () => {
 	};
 
 	return (
-		<div className='timer'>
+		<div className={classes.timer}>
 			<TextField
-				id='outlined-name'
-				label='Name of your task'
+				id='taskName'
+				placeholder='Name of your task'
+				color='primary'
 				size='small'
-				// onChange={handleChange}
-				className='taskName'
-				// inputProps={{ style: { textAlign: 'center' } }}
+				className={classes.taskName}
+				inputProps={{
+					className: classes.inputStyle,
+				}}
 			/>
 			<Paper
 				elevation={3}
-				className='timerClock'
+				className={classes.timerClock}
 				sx={{
 					borderRadius: 40,
 				}}
 			>
-				<Typography fontSize={30}>{taskTime}</Typography>
+				<Typography fontSize={30} color='primary'>
+					{taskTime}
+				</Typography>
 			</Paper>
-			{timerActive ? (
-				<Button variant='outlined' onClick={handleStopTimer}>
-					STOP
-				</Button>
-			) : (
-				<Button variant='outlined' onClick={handleStartTimer}>
-					START
-				</Button>
-			)}
+			<Button
+				variant='outlined'
+				className={classes.button}
+				onClick={timerActive ? handleStopTimer : handleStartTimer}
+			>
+				{timerActive ? 'STOP' : 'START'}
+			</Button>
 		</div>
 	);
 };
